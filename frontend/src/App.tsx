50628c80b7343f06dda9d3d8df0694e630213446
Lianna <liannapyman@gmail.com>
2024-08-19 14:16:46 +0930

    Restructures the project to separate frontend and backend server, adds basic server template with spring boot

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import Menu from "./components/Menu/Menu.tsx";
+import Menu from "./components/Menu/Menu";
 import QuestionCard from "./components/QuestionCard/QuestionCard";
 
 function App() {
---
+++ server/src/test/java/com/aivirt/triviaapi/TriviaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aivirt.triviaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriviaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
